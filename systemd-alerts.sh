75b69d6841fdeda97a0ac6282aef1cf4953c637e
spmfox <spmfox@foxwd.com>
2021-05-19 15:20:43 -0400

    Added comments and documentation.

--- systemd-alerts.sh
+++ systemd-alerts.sh
@@ -4,6 +4,9 @@
 
 #Script is designed to be run by systemd, with the first argument being the failed unit.
 
+# This script uses the following commands:
+# curl
+
 opt_FriendlyName=""                             # This name will appear in the message
 opt_TelegramBotToken=""                         # Token of Telegram Bot
 opt_TelegramMessageID=""                        # ID of the Telegram conversation to post the alert to
@@ -25,23 +28,41 @@ EOF
 )
 
 #Telegram message 
- if [ -n "$opt_TelegramBotToken" ] && [ -n "$opt_TelegramMessageID" ]; then
-  echo "Sending Telegram message."
-  str_CurlTelegram=$(curl -s -X POST -H "Content-Type: application/json" -d "$str_GenerateJSON" https://api.telegram.org/bot$opt_TelegramBotToken/sendMessage)
- else
-  echo "One or both Telegram settings are missing, not sending Telegram message."
- fi
+if [ -n "$opt_TelegramBotToken" ] && [ -n "$opt_TelegramMessageID" ]; then
+ echo "Sending Telegram message."
+ str_CurlTelegram=$(curl -s -X POST -H "Content-Type: application/json" -d "$str_GenerateJSON" https://api.telegram.org/bot$opt_TelegramBotToken/sendMessage)
+else
+ echo "One or both Telegram settings are missing, not sending Telegram message."
+fi
 
 #Email message
- if [ -n "$opt_EmailServer" ] && [ -n "$opt_EmailFrom" ] && [ -n "$opt_EmailTo" ] && [ -n "$opt_EmailUser" ] && [ -n "$opt_EmailPassword" ]; then
-  echo "Sending email."
-  echo "From: $opt_EmailFrom" > $file_AlertEmail
-  echo "To: $opt_EmailTo" >> $file_AlertEmail
-  echo "Subject: $opt_FriendlyName $str_FailedUnit Alert" >> $file_AlertEmail
-  echo "" >> $file_AlertEmail
-  echo $str_FailedUnitJournal >> $file_AlertEmail
-  str_CurlEmail=$(curl -s --ssl-reqd smtp://$opt_EmailServer --mail-from $opt_EmailFrom --mail-rcpt $opt_EmailTo --upload-file $file_AlertEmail --user "$opt_EmailUser":"$opt_EmailPassword")
-  rm $file_AlertEmail
- else
-  echo "One or more email settings are missing, not sending email message."
- fi
+if [ -n "$opt_EmailServer" ] && [ -n "$opt_EmailFrom" ] && [ -n "$opt_EmailTo" ] && [ -n "$opt_EmailUser" ] && [ -n "$opt_EmailPassword" ]; then
+ echo "Sending email."
+ echo "From: $opt_EmailFrom" > $file_AlertEmail
+ echo "To: $opt_EmailTo" >> $file_AlertEmail
+ echo "Subject: $opt_FriendlyName $str_FailedUnit Alert" >> $file_AlertEmail
+ echo "" >> $file_AlertEmail
+ echo $str_FailedUnitJournal >> $file_AlertEmail
+ str_CurlEmail=$(curl -s --ssl-reqd smtp://$opt_EmailServer --mail-from $opt_EmailFrom --mail-rcpt $opt_EmailTo --upload-file $file_AlertEmail --user "$opt_EmailUser":"$opt_EmailPassword")
+ rm $file_AlertEmail
+else
+ echo "One or more email settings are missing, not sending email message."
+fi
+
+#Sample systemd file:
+###
+#alert-notification@.service
+#
+#[Unit]
+#Description=triggering alert for %i
+#
+#[Service]
+#Type=oneshot
+#ExecStart=/usr/bin/bash /opt/systemd-alerts.sh %i
+###
+
+#Example using this in another systemd unit:
+###
+#[Unit]
+#OnFailure=alert-notification@%i.service
+###
